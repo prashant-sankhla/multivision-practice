1802c6bf75a813be56dbe888f000f5aa37f4add8
joeeames <joeeames@gmail.com>
2013-12-04 00:39:07 -0700

    implemented profile editing

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -25,4 +25,27 @@ exports.createUser = function(req, res, next) {
       res.send(user);
     })
   })
-}
+};
+
+exports.updateUser = function(req, res) {
+  var userUpdates = req.body;
+
+  if(req.user._id != userUpdates._id && !req.user.hasRole('admin')) {
+    res.status(403);
+    return res.end();
+  }
+
+  // NEED TO LOOK UP USER IF NOT THE SAME ID AS CURRENT USER AND USER IS ADMIN,
+  // LEAVE TO USER TO IMPLEMENT
+  req.user.firstName = userUpdates.firstName;
+  req.user.lastName = userUpdates.lastName;
+  req.user.username = userUpdates.username;
+  if(userUpdates.password && userUpdates.password.length > 0) {
+    req.user.salt = encrypt.createSalt();
+    req.user.hashed_pwd = encrypt.hashPwd(req.user.salt, userUpdates.password);
+  }
+  req.user.save(function(err) {
+    if(err) { res.status(400); return res.send({reason:err.toString()});}
+    res.send(req.user);
+  });
+};
